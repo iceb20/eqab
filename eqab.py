2b2b6fdb82012f766d862229711bc408bda66903
Eqab <77283152+iceb20@users.noreply.github.com>
2025-01-28 04:09:56 +0300

    Save eqab.py.

--- eqab.py
+++ eqab.py
@@ -1,18 +1,18 @@
 from telegram import Bot
 from telegram.ext import Updater, CommandHandler
 
-API_TOKEN = '1922426592:AAE9Z5qIVVh97Gx8ow2gE8zxyd7CD1GIG2M'
+# استبدل TOKEN الخاص بك
+TOKEN = '1922426592:AAE9Z5qIVVh97Gx8ow2gE8zxyd7CD1GIG2M'
 
-# دالة لبدء المحادثة مع البوت
 def start(update, context):
-    update.message.reply_text("مرحباً! تم اختبار الكود بنجاح!")
+    update.message.reply_text('مرحبًا! البوت يعمل.')
 
 def main():
-    updater = Updater(API_TOKEN, use_context=True)
+    updater = Updater(TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
-    # إضافة معالج للأوامر
-    dispatcher.add_handler(CommandHandler("start", start))
+    # إضافة معالج للأمر '/start'
+    dispatcher.add_handler(CommandHandler('start', start))
 
     # بدء البوت
     updater.start_polling()
